6a116c3bc33afefac6ea89d5a8c512538c29b898
progriro <progriro@gmail.com>
2020-10-18 00:38:18 +0900

    :white_check_mark: テストコードの追加 :white_check_mark:

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
+const toFizzBuzz = require("./modules/fizzbuzz");
+
 const app = () => {
   for (let i = 1; i <= 100; i++) {
     console.log(toFizzBuzz(i));
   }
-};
-
-const toFizzBuzz = (num) => {
-  if (i % 3 == 0 && i % 5 == 0) return "FizzBuzz";
-  if (i % 3 == 0) return "Fizz";
-  if (i % 5 == 0) return "Buzz";
-  return num;
 };
 
 module.exports = app;
