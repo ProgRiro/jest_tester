38f62835dad4f5b5366d69b34430a0b6134bea7a
progriro <progriro@gmail.com>
2020-10-18 00:26:38 +0900

    :art: app.js リファクタ :art:

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const app = () => {
   for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
-      console.log("FizzBuzz");
-      continue;
-    }
-
-    if (i % 3 == 0) {
-      console.log("Fizz");
-      continue;
-    }
-
-    if (i % 5 == 0) {
-      console.log("Buzz");
-      continue;
-    }
-
-    console.log(i);
+    console.log(toFizzBuzz(i));
   }
+};
+
+const toFizzBuzz = (num) => {
+  if (i % 3 == 0 && i % 5 == 0) return "FizzBuzz";
+  if (i % 3 == 0) return "Fizz";
+  if (i % 5 == 0) return "Buzz";
+  return num;
 };
 
 module.exports = app;
