eafda58005af610424dd816b53b2ec80d708ba5c
progriro <progriro@gmail.com>
2021-01-21 22:37:39 +0900

    [modified] webpack confing

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,49 @@
 const path = require("path");
-
 module.exports = {
   mode: "development",
-  devtool: "none",
-  entry: "./src/app.ts",
+  entry: path.join(__dirname, "src", "app"),
+  watch: true,
   output: {
+    path: path.join(__dirname, "public"),
+    publicPath: "/public/",
     filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
+    chunkFilename: "[name].js",
   },
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/,
+        test: /\.tsx?$/,
         use: ["ts-loader"],
         exclude: [/node_modules/],
       },
+      {
+        test: /\.(jsx?|tsx?)$/,
+        include: [path.resolve(__dirname, "src")],
+        exclude: [path.resolve(__dirname, "node_modules")],
+        loader: "babel-loader",
+        query: {
+          presets: [
+            [
+              "@babel/env",
+              {
+                targets: {
+                  browsers: "last 2 chrome versions",
+                },
+              },
+            ],
+          ],
+        },
+      },
     ],
+  },
+  resolve: {
+    extensions: [".json", ".js", ".jsx"],
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "/public/"),
+    inline: true,
+    host: "localhost",
+    port: 8080,
   },
 };
