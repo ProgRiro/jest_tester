b061d8007d80b8e849c985d7d0dc4110decb97ba
progriro <progriro@gmail.com>
2021-01-21 22:41:01 +0900

    [fixed] entry path

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 module.exports = {
   mode: "development",
-  entry: path.join(__dirname, "src", "app"),
+  entry: path.join(__dirname, "src", "app.ts"),
   watch: true,
   output: {
     path: path.join(__dirname, "public"),
